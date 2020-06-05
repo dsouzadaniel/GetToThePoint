20928f1052146a5db04426e245878c887c3fb3ca
danieldsouza <ddsouza@umich.edu>
2020-06-04 20:32:25 -0400

    # Add Encoder( Bi-Lstm)

--- model/architecture.py
+++ model/architecture.py
@@ -14,7 +14,7 @@ class BaselineSeq2Seq2wAttn(nn.Module):
 
         # Model Properties
         self.elmo_sent = elmo_sent
-
+        self.randomize_init_hidden = True
 
         # Model Constants
         self.ELMO_EMBED_DIM = 256  # This will change if the ELMO options/weights change
@@ -23,6 +23,10 @@ class BaselineSeq2Seq2wAttn(nn.Module):
 
         # Model Layers
         self.elmo = Elmo(self.OPTIONS_FILE, self.WEIGHTS_FILE, 1)
+        self.encoder = nn.LSTM(input_size=self.ELMO_EMBED_DIM,
+                               hidden_size=self.ELMO_EMBED_DIM,
+                               num_layers=1,
+                               bidirectional=True)
 
     def _elmo_embed_doc(self, doc_tokens: List[List[str]]) -> torch.Tensor:
         if not self.elmo_sent:
@@ -48,14 +52,38 @@ class BaselineSeq2Seq2wAttn(nn.Module):
         doc_embedded_elmo = self._elmo_embed_doc(doc_tokens)
         return doc_embedded_elmo
 
-    def forward(self, x:str) -> torch.Tensor:
-        return self._embed_doc(x)
+    def _init_bi_hidden(self, batch_size: int = 1, num_layers: int = 1):
+        if self.randomize_init_hidden:
+            init_hidden = torch.randn(num_layers * 2, batch_size,
+                                      self.ELMO_EMBED_DIM)
+        else:
+            init_hidden = torch.zeros(num_layers * 2, batch_size,
+                                      self.ELMO_EMBED_DIM)
+        return init_hidden, init_hidden
+
+    def _run_through_bilstm(self, input_tensor: torch.Tensor, bilstm: torch.nn.modules.rnn):
+        init_bi_hidden = self._init_bi_hidden(num_layers=bilstm.num_layers)
+        output_tensor, _ = bilstm(input_tensor, init_bi_hidden)
+        output_tensor = output_tensor.view(input_tensor.shape[0], 1, 2, bilstm.hidden_size)
+        output_tensor = torch.cat((output_tensor[:, :, 0, :],
+                                   output_tensor[:, :, 1, :]),
+                                  dim=2).squeeze(dim=1)
+        return output_tensor
+
+    def forward(self, x: str) -> torch.Tensor:
+        # Embed the Doc with Elmo
+        doc_embedded_elmo = self._embed_doc(x)
+        # Encode with BiLSTM
+        doc_embedded_elmo = doc_embedded_elmo.unsqueeze(dim=1)
+        encoder_states = self._run_through_bilstm(doc_embedded_elmo, self.encoder)
+
+        return encoder_states
 
 
 model = BaselineSeq2Seq2wAttn()
 
-input_text = "Hello World. This is great"
+input_text = "Hello World. This is great. I love NLP"
 
-output_tensor = model(input_text)
+tensor = model(input_text)
 
-print("Output Tensor Shape is :{0}".format(output_tensor.shape))
+print("Output Tensor Shape is :{0}".format(tensor.shape))
