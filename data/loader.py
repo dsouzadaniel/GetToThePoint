12b369a35971553ddade4d39a641f84777140f3b
danieldsouza <ddsouza@umich.edu>
2020-08-10 14:43:49 -0400

    # Adding dataset link

--- data/loader.py
+++ data/loader.py
@@ -5,8 +5,8 @@ from torch.utils.data import Dataset
 class CNNLoader(Dataset):
 
     def __init__(self, path_to_csv):
-        df = pd.read_csv(path_to_csv, nrows=100)
-        self.data_tuples = [(r['Original'][:100], r['Summary'][:100]) for _, r in df.iterrows()]
+        df = pd.read_csv(path_to_csv)
+        self.data_tuples = [(r['Original'], r['Summary']) for _, r in df.iterrows()]
 
     def __getitem__(self, item):
         return self.data_tuples[item]
